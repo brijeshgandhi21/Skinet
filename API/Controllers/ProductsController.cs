a30303a0861a3d9c93f57dff82aa8d01ef003ccd
Brijesh Gandhi <brijeshgandhi21@gmail.com>
2020-08-19 23:03:28 -0400

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() 
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id) => "This will be list of products";
+
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
